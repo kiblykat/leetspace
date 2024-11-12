d3db160dfd90f883b4f5d59b1bd4ea429c25da04
izzat <izzatfadzlon@gmail.com>
2024-11-12 18:16:56 +0800

    fe: barebones frontend, add jsx to tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,35 @@ function App() {
 
   return (
     <div className="App">
-      <button className="btn" onClick={fetchQuestions}>
+      <button className="btn m-2" onClick={fetchQuestions}>
         getQuestions
       </button>
+      <div className="flex-col align-bottom justify-start">
+        <div>
+          <label>title</label>
+          <input></input>
+        </div>
+        <div>
+          <label className="p-2 m-2">link</label>
+          <input className="p-2 m-2"></input>
+        </div>
+        <div className="p-2 m-2">
+          <label>notes</label>
+          <input></input>
+        </div>
+        <div className="p-2 m-2">
+          <label>difficulty</label>
+          <input></input>
+        </div>
+        <div className="p-2 m-2">
+          <label>topic</label>
+          <input></input>
+        </div>
+        <div className="p-2 m-2">
+          <label>tags</label>
+          <input></input>
+        </div>
+      </div>
     </div>
   );
 }
