a896df9d8c75d9e364d73ae5556b07b6bd6beaa1
kiblykat <izzatfadzlon@gmail.com>
2024-11-23 11:55:00 +0800

    fe: add react-router-dom and move Toaster component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import NewQuestion from "./components/NewQuestion";
 import QuestionList from "./components/QuestionList";
-import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <NewQuestion />
       <QuestionList />
-      <Toaster  />
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { QuestionProvider } from "./contexts/QuestionContext.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QuestionProvider>
       <App />
+      <Toaster />
     </QuestionProvider>
   </StrictMode>
 );
