029cfd4e08faf65d45c48662c5bd1895568ca13e
kiblykat <izzatfadzlon@gmail.com>
2024-11-24 22:23:56 +0800

    fe: add Login page and route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, HashRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Bank from "./pages/Bank";
 import RootLayout from "./layouts/RootLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<RootLayout />}>
             <Route path="/home" element={<Home />} />
             <Route path="/bank" element={<Bank />} />
+            <Route path="/login" element={<Login />} />
           </Route>
         </Routes>
       </HashRouter>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <div>
+      <div className="container inset-0 mx-auto shadow-lg text-center">
+        Login
+      </div>
+    </div>
+  );
+};
+
+export default Login;
