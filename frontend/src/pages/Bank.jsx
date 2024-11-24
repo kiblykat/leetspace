7ce318cb9739815247d114eb97bbad30c514762d
kiblykat <izzatfadzlon@gmail.com>
2024-11-24 10:56:19 +0800

    fe: add search functionality to Revision Bank

--- frontend/src/pages/Bank.jsx
+++ frontend/src/pages/Bank.jsx
@@ -4,7 +4,10 @@ import { useNavigate } from "react-router-dom";
 
 const Bank = () => {
   const [bankQuestions, setBankQuestions] = useState([]);
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
+
+  let handleSearch = (e) => {};
 
   let getRevisionBank = async () => {
     try {
@@ -25,6 +28,14 @@ const Bank = () => {
       <div className="container mx-auto my-10 p-6 bg-base-200 rounded shadow-lg flex-col">
         <h1 className="text-3xl font-bold mb-6 text-center">Revision Bank</h1>
         <div className="rounded shadow-lg flex flex-col p-6 ">
+          <input
+            type="text"
+            name="title"
+            className="input input-bordered"
+            placeholder="Search for Questions"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <div className="flex align-middle justify-center"></div>
           <table className="table table-auto w-full">
             {/* Table Head */}
@@ -40,19 +51,23 @@ const Bank = () => {
             </thead>
             {/* Table Body */}
             <tbody>
-              {bankQuestions.map((question, index) => (
-                <tr
-                  key={question._id}
-                  className="hover:bg-base-300 btn-ghost cursor-pointer z-10"
-                >
-                  <td>{index + 1}</td>
-                  <td>{question.title}</td>
-                  <td>{question.tags}</td>
-                  <td>{question.difficulty}</td>
-                  <td>{question.reviewDate}</td>
-                  <td>{question.reviewCount}</td>
-                </tr>
-              ))}
+              {bankQuestions
+                .filter((question) =>
+                  question.title.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((question, index) => (
+                  <tr
+                    key={question._id}
+                    className="hover:bg-base-300 btn-ghost cursor-pointer z-10"
+                  >
+                    <td>{index + 1}</td>
+                    <td>{question.title}</td>
+                    <td>{question.tags}</td>
+                    <td>{question.difficulty}</td>
+                    <td>{question.reviewDate}</td>
+                    <td>{question.reviewCount}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
