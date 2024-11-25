2d118fff68c2df1c24f9c3a7b6700aa915cadea5
kiblykat <izzatfadzlon@gmail.com>
2024-11-25 23:52:15 +0800

    fe: enhance Login component styling and update title format

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -32,8 +32,10 @@ const Login = () => {
   return (
     <div className="container mx-auto p-6 bg-base-200 rounded shadow-lg flex flex-col justify-center items-center min-h-screen">
       {userLoggedIn && navigate("/home")}
-      <div className="bg-base-300 m-5- p-24 shadow-2xl rounded-xl">
-        <h1 className="text-3xl font-bold mb-6 text-center">LeetSpace</h1>
+      <div className="bg-base-300 m-5- p-24 shadow-2xl rounded-xl flex flex-col justify-center">
+        <h1 className="text-3xl font-bold mb-6 text-center font-mono">
+          LeetSpace();
+        </h1>
         <button
           onClick={handleGoogleLogin}
           className="btn bg-orange-200 text-black hover:bg-orange-400"
