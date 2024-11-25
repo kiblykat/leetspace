9eacebd419304850dee4f897af7cd2b2497b3bbd
kiblykat <izzatfadzlon@gmail.com>
2024-11-25 16:03:44 +0800

    fe: implement Google sign-in functionality and simplify Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,67 +1,32 @@
 import { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  let navigate = useNavigate();
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const handleGoogleLogin = async (e) => {
     try {
-      console.log("submit success");
+      const provider = new GoogleAuthProvider();
+      return await signInWithPopup(auth, provider);
     } catch (err) {
       setError("Login failed. Please try again. ");
-      console.log(err);
     }
   };
 
   return (
-    <div className="container mx-auto p-6 bg-base-200 rounded shadow-lg">
-      <h1 className="text-3xl font-bold mb-6 text-center">Login</h1>
-      <form
-        className="flex flex-col p-6 rounded shadow-lg"
-        onSubmit={handleSubmit}
-      >
-        <div className="form-control mb-4">
-          <label className="label font-bold text-xl">Email:</label>
-          <input
-            type="email"
-            name="email"
-            className="input input-bordered"
-            placeholder="Enter your email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-control mb-4">
-          <label className="label font-bold text-xl">Password:</label>
-          <input
-            type="password"
-            name="password"
-            className="input input-bordered"
-            placeholder="Enter your password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+    <div className="container mx-auto p-6 bg-base-200 rounded shadow-lg flex flex-col justify-center items-center min-h-screen">
+      {userLoggedIn && navigate("/home")}
+      <div className="bg-base-300 m-5- p-24 shadow-2xl rounded-xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">LeetSpace</h1>
         <button
-          type="submit"
-          className="btn bg-orange-300 text-black hover:bg-orange-400"
-          onClick={handleSubmit}
+          onClick={handleGoogleLogin}
+          className="btn bg-orange-200 text-black hover:bg-orange-400"
         >
-          Login
+          Sign In with Google
         </button>
-      </form>
+      </div>
     </div>
   );
 };
