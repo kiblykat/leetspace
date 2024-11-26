8ecae32f369950a8aae442c3ccd0df44634a3188
kiblykat <izzatfadzlon@gmail.com>
2024-11-26 16:26:02 +0800

    fe: redirect to home on successful login using useEffect

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
@@ -29,9 +29,17 @@ const Login = () => {
     }
   };
 
+  useEffect(
+    (userLoggedIn) => {
+      if (userLoggedIn) {
+        navigate("/home");
+      }
+    },
+    [userLoggedIn]
+  );
+
   return (
     <div className="container mx-auto p-6 bg-base-200 rounded shadow-lg flex flex-col justify-center items-center min-h-screen">
-      {userLoggedIn && navigate("/home")}
       <div className="bg-base-300 m-5- p-24 shadow-2xl rounded-xl flex flex-col justify-center">
         <h1 className="text-3xl font-bold mb-6 text-center font-mono">
           LeetSpace();
