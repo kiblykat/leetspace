f565829582a48aad266e944a7362202104422ecd
kiblykat <izzatfadzlon@gmail.com>
2024-11-26 10:49:11 +0800

    fix: vercel- update API base URL to production endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-let BASE_URL = "http://localhost:5000";
+let BASE_URL = "https://leetspace-api.vercel.app/";
+// let BASE_URL = "http://localhost:5000";
 let questionApi = axios.create({ baseURL: BASE_URL });
 
 export default questionApi;
