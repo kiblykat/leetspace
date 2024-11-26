687eee73eae5453be79f048182d53453e5e24f67
kiblykat <izzatfadzlon@gmail.com>
2024-11-26 09:28:17 +0800

    fe: redirect to login on root path in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, HashRouter, Route } from "react-router-dom";
+import { Routes, HashRouter, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Bank from "./pages/Bank";
 import RootLayout from "./layouts/RootLayout";
 import Login from "./pages/Login";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<RootLayout />}>
+            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/home" element={<Home />} />
             <Route path="/bank" element={<Bank />} />
             <Route path="/login" element={<Login />} />
