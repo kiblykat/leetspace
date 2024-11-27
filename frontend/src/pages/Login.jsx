91ef5a6151eb94603ca64390fa6126f0926e7b9c
kiblykat <izzatfadzlon@gmail.com>
2024-11-27 23:18:42 +0800

    fe: add mono subtitle to Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -46,9 +46,10 @@ const Login = () => {
   return (
     <div className="container mx-auto p-6 bg-base-200 rounded shadow-lg flex flex-col justify-center items-center min-h-screen">
       <div className="bg-base-300 m-5- p-24 shadow-2xl rounded-xl flex flex-col justify-center">
-        <h1 className="text-3xl font-bold mb-6 text-center font-mono">
+        <h1 className="text-3xl font-bold mb-2 text-center font-mono">
           LeetSpace();
         </h1>
+        <p className="font-mono mb-8">leetcode through repetition</p>
         <button
           onClick={handleGoogleLogin}
           className="btn bg-orange-200 text-black hover:bg-orange-400"
