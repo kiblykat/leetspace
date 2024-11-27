b2b83526aefbbb6dd5b3381df04a6025f1725b1c
kiblykat <izzatfadzlon@gmail.com>
2024-11-27 23:55:44 +0800

    fe: add social media links to Login page and include Font Awesome script

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
 
   return (
     <div className="container mx-auto p-6 bg-base-200 rounded shadow-lg flex flex-col justify-center items-center min-h-screen">
-      <div className="bg-base-300 m-5- p-24 shadow-2xl rounded-xl flex flex-col justify-center">
+      <div className="bg-base-300 m-5 p-24 shadow-2xl rounded-xl flex flex-col justify-center relative">
         <h1 className="text-3xl font-bold mb-2 text-center font-mono">
           LeetSpace();
         </h1>
@@ -56,6 +56,21 @@ const Login = () => {
         >
           Sign In with Google
         </button>
+      </div>
+      <div className="absolute bottom-0 right-0 p-4 text-gray-500 flex flex-row items-center">
+        <span className="text-center font-mono">Contact me: </span>
+        <a
+          href="https://github.com/kiblykat"
+          target="_blank"
+          rel="noreferrer"
+          className="fa-brands fa-github text-3xl mx-2 hover:text-orange-300 transition-transform transform hover:scale-110"
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/izzat-fadzlon-75615b171/"
+          target="_blank"
+          rel="noreferrer"
+          className="fa-brands fa-linkedin text-3xl mx-2 hover:text-orange-300 transition-transform transform hover:scale-110"
+        ></a>
       </div>
     </div>
   );
